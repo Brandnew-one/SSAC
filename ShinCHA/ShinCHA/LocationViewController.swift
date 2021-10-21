8b154a6a2950f2a2eae0d429582f661ea0013959
Sangwon <cold929@naver.com>
2021-10-21 15:02:08 +0900

    사용자가 위치설정을 허용하지 않은 경우 Alert 을 통해 iOS 설정화면으로 유도

--- ShinCHA/ShinCHA/LocationViewController.swift
+++ ShinCHA/ShinCHA/LocationViewController.swift
@@ -59,33 +59,6 @@ class LocationViewController: UIViewController {
     }
     
     func filterTheater(_ index: Int) {
-        
-//        //각각 나오게 하는 코드 구현
-//        if !boolButton[index] {
-//            boolButton[index] = true
-//            for annotation in mapView.annotations {
-//                if annotation.subtitle == refButton[index] {
-//                    mapView.removeAnnotation(annotation)
-//                }
-//            }
-//        }
-//
-//        else {
-//            boolButton[index] = false
-//            for info in theater {
-//                if info.type == refButton[index] {
-//                    let latitude = info.latitude
-//                    let longtitude = info.longitude
-//                    let location = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
-//
-//                    let annotation = MKPointAnnotation()
-//                    annotation.title = info.location
-//                    annotation.coordinate = location
-//                    annotation.subtitle = info.type //annotation 클릭해야 나오는거 확인함 -> 이걸 tag 처럼 사용할 계획
-//                    mapView.addAnnotation(annotation)
-//                }
-//            }
-//        }
         //전체보기 누른경우 -> 전체 화면 다 추가
         if index == 3 {
             mapView.removeAnnotations(mapView.annotations)
@@ -100,7 +73,6 @@ class LocationViewController: UIViewController {
                 annotation.subtitle = info.type //annotation 클릭해야 나오는거 확인함 -> 이걸 tag 처럼 사용할 계획
                 mapView.addAnnotation(annotation)
             }
-            
         }
         
         else {
@@ -118,9 +90,7 @@ class LocationViewController: UIViewController {
                     mapView.addAnnotation(annotation)
                 }
             }
-            
         }
-        
     }
     
     //Alert 구현
@@ -130,8 +100,7 @@ class LocationViewController: UIViewController {
         alertController = UIAlertController(title: "Filter", message: "영화관을 선택하세요", preferredStyle: style)
         
         let lotteAction: UIAlertAction
-        // handler는 alert action이 선택됐을때, OK버튼이 실행된다면
-        // 실행될 코드 블럭을 의미한다
+    
         lotteAction = UIAlertAction(title: "LOTTE", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction) in self.filterTheater(0)
         })
         
@@ -161,13 +130,9 @@ class LocationViewController: UIViewController {
         // 모달로 올려줌 -> completion은 모달이 올라오는 애니메이션이 끝나고 직후에 호출될 블럭
         self.present(alertController, animated: true, completion: { print("Alert controller shown") })
         
-        
     }
-    
-    
     @IBOutlet weak var mapView: MKMapView! //extension 잊지말자!
     
-
 }
 
 // 2)
@@ -192,7 +157,19 @@ extension LocationViewController: CLLocationManagerDelegate {
             //권한 상태 확인 및 권한 요청가능 -> 8번 메서드 실행
             checkCurrentLocationAuthorization(authorizationStatus)
         }
+        //아예 시스템에서 위치 기능을 끈 경우
         else {
+            showAlert(title: "설정으로 이동", message: "권한 허용해주세요", okTitle: "설정으로 가기") {
+                guard let url = URL(string: UIApplication.openSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url) { success in
+                        print("잘 열렸다. \(success)")
+                    }
+                }
+            }
             print("iOS 위치 서비스를 켜주세요!")
         }
         
@@ -209,6 +186,17 @@ extension LocationViewController: CLLocationManagerDelegate {
             locationManager.startUpdatingLocation() // 위치 접근 시작! (같이 나와야함)
         case .restricted, .denied:
             print("Denied, 설정으로 유도")
+            showAlert(title: "설정으로 이동", message: "권한 허용해주세요", okTitle: "설정으로 가기") {
+                guard let url = URL(string: UIApplication.openSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url) { success in
+                        print("잘 열렸다. \(success)")
+                    }
+                }
+            }
             
         case .authorizedWhenInUse:
             locationManager.startUpdatingLocation()
@@ -241,13 +229,14 @@ extension LocationViewController: CLLocationManagerDelegate {
         
         print("감사합니다. 허용해 주셔서")
         
+        // 사용자가 앱 내에서 위치 허용을 해줬으니까 현재 위치를 지도의 중심으로 설정한다.
         if let coordinate = locations.last?.coordinate {
             let annotation = MKPointAnnotation()
-            annotation.title = "Current Location"
+            annotation.title = "나 여기"
             annotation.coordinate = coordinate
             mapView.addAnnotation(annotation)
             
-            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
             let region = MKCoordinateRegion(center: coordinate, span: span)
             mapView.setRegion(region, animated: true)
             
@@ -256,27 +245,26 @@ extension LocationViewController: CLLocationManagerDelegate {
             
         }
         
+        //얘기치 못한 오류 방지 (비행기 모드로 변환되었을 경우) <-> 5번함수와의 기능
         else {
             print("Location Cannot Find")
         }
-        
     }
     
-    // 5) 위치 접근에 실패한 경우 (여기에 사용자가 앱내 위치 정보를 거부한것도 포함되는건가?)
+    // 5) 위치 접근에 실패한 경우 (여기에 사용자가 앱내 위치 정보를 거부한것도 포함되는건가? ㅇ)
+    // 위치접근을 허용했으나 위치 정보 조회에 실패한 경우 지도에서 어떤화면을 보여줄 것인가?
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
         print("허용 안했어? 왜 오류가 나!")
         
         //위치 허용하지 않은 경우, 건국대 근처 스터디 카페
         let location = CLLocationCoordinate2D(latitude: 37.541366, longitude: 127.067187)
-        //설정한 위치에서 얼만큼 보여줄건지?
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
         
         let annotation = MKPointAnnotation()
-        //let annotations = mapView.annotations
-        annotation.title = "나"
+        annotation.title = "눈물의 스터디 현장"
         annotation.coordinate = location
         mapView.addAnnotation(annotation)
     }
@@ -292,9 +280,6 @@ extension LocationViewController: CLLocationManagerDelegate {
         print("승인상태 변경")
         checkUserLocationServicesAuthorization()
     }
-    
-    
-    
     
 }
 
