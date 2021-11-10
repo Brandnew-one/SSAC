64f0f0fa55fcfb7b0796e1b11ea5728e1e4bec0f
Sangwon <cold929@naver.com>
2021-11-10 09:15:54 +0900

    test

---
+++ week6/week6/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCollectionViewCell.swift
+//  week6
+//
+//  Created by 신상원 on 2021/11/08.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ week6/week6/Home/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  week6
+//
+//  Created by 신상원 on 2021/11/08.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
